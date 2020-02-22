7d7d41c3407f7568afada455418de1213616e01a
Dorian Calin <dorin.m.calin@gmail.com>
2020-02-22 20:04:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -26,7 +26,7 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-    
+    source_array = [1, 2, true, "razmatazz"]
 end
 
 def reduce_to_any_true(source_array)
